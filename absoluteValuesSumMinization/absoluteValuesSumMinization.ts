140c78cf4c8020a98dfd15d3fabb2cfde26834c1
Ketan Shukla <ketankshukla@gmail.com>
2020-06-12 08:19:08 -0700

    version 1 of absoluteValuesSumMinimization

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,15 @@
-function absoluteValuesSumMinimization(a: number[]): number {
+const absoluteValuesSumMinimization = (a: number[]): number => {
+
+    const half = halfway(a)
+    return (a.length % 2) ? a[half] :  a[half-1]
 
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+const halfway = (a: number[]): number => Math.floor(a.length/2)
+
+halfway
+
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
